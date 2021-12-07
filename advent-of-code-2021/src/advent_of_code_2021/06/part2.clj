ba83f27591f26bf5448db2f98a9edcb1aa234286
Zach Fedor <zachfedor@gmail.com>
2021-12-07 01:13:45 -0500

    Day 6 - part 1

--- advent-of-code-2021/src/advent_of_code_2021/06/part2.clj
+++ advent-of-code-2021/src/advent_of_code_2021/06/part2.clj
@@ -1,5 +1,35 @@
 (ns advent-of-code-2021.06.part2
   (:require [advent-of-code-2021.06.part1 :as part1
-             :refer [example input]]))
+             :refer [example input]]
+            [clojure.string :as str]))
 
-example
+(defn build-freq
+  "Given string of timers, build a vector containing the frequencies of each possible day
+  `[0 n0, 1 n1, ... 8 n8]` where nX is the number of fish at day x"
+  [in]
+  (vec (for [x (range 9)]
+         (get (frequencies (map #(Integer/parseInt %) (str/split in #","))) x 0))))
+
+(defn update-timers
+  "Given a vector of timers, return an updated map for the next cycle"
+  [timers]
+  (let [new (nth timers 0)] ;; Save the amount of new fish to be created
+    (as-> timers in
+        (rest in) ;; Decrement index of each timer by removing the first item
+        (vec in) ;; Convert rest list back into vector
+        (assoc in 6 (+ (nth in 6) new)) ;; Reset the day 0 fish, adding them to day 6
+        (conj in new)))) ;; Add newly created fish to end of vector as day 8s
+
+;; (update-timers (build-freq example))
+
+(defn run [initial days]
+  (def days-left (atom days))
+  (def timers (atom (build-freq initial)))
+
+  (while (pos? @days-left)
+    (swap! timers update-timers)
+    (swap! days-left dec))
+
+  (reduce + @timers))
+
+(println (run input 256))
