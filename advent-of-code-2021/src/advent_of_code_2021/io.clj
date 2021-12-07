d57fbb4b3ebe18e8057aab49ae006c20e311bda3
Zach Fedor <zachfedor@gmail.com>
2021-12-07 01:13:13 -0500

    Added more input file reading utilities

--- advent-of-code-2021/src/advent_of_code_2021/io.clj
+++ advent-of-code-2021/src/advent_of_code_2021/io.clj
@@ -1,4 +1,5 @@
-(ns advent-of-code-2021.io)
+(ns advent-of-code-2021.io
+  (:require [clojure.string :as str]))
 
 (defn readInput
   "Given a file, return it's contents as a vector of lines as strings"
@@ -9,4 +10,17 @@
 (defn readInputN
   "Given a file, return it's contents as a vector of lines as integers"
   [file]
-  (map #(Integer/parseInt %) (readInput file)))
+  (vec (map #(Integer/parseInt %) (readInput file))))
+
+(defn readStrings
+  "Given a file, return its single-line contents as a vector of strings, split by a separator"
+  ([file] (readStrings file #","))
+  ([file separator] (-> file
+                        (readInput)
+                        (first)
+                        (str/split separator))))
+
+(defn readInts
+  "Given a file, return its single-line contents as a vector of integers, split by a separator"
+  ([file] (readInts file #","))
+  ([file separator] (vec (map #(Integer/parseInt %) (readStrings file separator)))))
